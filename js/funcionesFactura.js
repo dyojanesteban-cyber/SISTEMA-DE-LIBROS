b247b10d163c76cd1a52e0ff1997e5551a7face4
Vann <navascinthya13@gmail.com>
2025-12-10 11:46:38 -0500

    funciones con javascript

---
+++ js/funcionesFactura.js
@@ -0,0 +1,33 @@
+var total = 0;
+var numProductos = 0;
+
+function addFila() {
+    var tabla = document.getElementById("tblFactura").getElementsByTagName("tbody") [0];
+    console.log(tabla);
+    var fila = tabla.insertRow(-1);
+
+    var celda1 = fila.insertCell(0);
+    var celda2 = fila.insertCell(1);
+    var celda3 = fila.insertCell(2);
+    var celda4 = fila.insertCell(3);
+    var celda5 = fila.insertCell(4);
+    var celda6 = fila.insertCell(5);
+
+var botonEliminar =document.createElement("button");
+botonEliminar .innerHTML ="-"
+botonEliminar .onclick = function() {
+    var row = this.parentNode.parentNode;
+    row.remove();
+    numProductos--;
+    var contador = document. getElementById("numProductos");
+    contador.innerHTML = "<b>"+numProductos+"</b>";
+
+}
+celda1 .appendChild(botonEliminar);
+
+numProductos++;
+
+var contador = document. getElementById("numProductos");
+contador.innerHTML = "<b>"+numProductos+"</b>";
+
+}    
